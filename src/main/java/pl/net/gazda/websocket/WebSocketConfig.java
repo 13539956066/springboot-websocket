f7119d3decea054b1ebda98163de64fb9e6676c8
lgazda <lukasz@gazda.net.pl>
2017-07-25 10:08:52 +0200

    synchronized fix

--- src/main/java/pl/net/gazda/websocket/WebSocketConfig.java
+++ src/main/java/pl/net/gazda/websocket/WebSocketConfig.java
@@ -62,15 +62,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public static class WebSocketSessions {
         Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
 
-        public synchronize void add(WebSocketSession session) {
+        public synchronized void add(WebSocketSession session) {
             sessions.put(session.getId(), session);
         }
 
-        public synchronize void remove(WebSocketSession session) {
+        public synchronized void remove(WebSocketSession session) {
             sessions.remove(session.getId());
         }
 
-        public synchronize void sendMessage(TextMessage message) throws IOException {
+        public synchronized void sendMessage(TextMessage message) throws IOException {
             for (String s : sessions.keySet()) {
                 sessions.get(s).sendMessage(message);
             }
